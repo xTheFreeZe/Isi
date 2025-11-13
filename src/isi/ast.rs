030a9f1fb26bdc7bc324f8a90d91529d85302b0c
xTheFreeZe <marwin.eder@gmx.de>
2025-11-13 23:45:41 +0100

    Start on Ast

--- src/isi/ast.rs
+++ src/isi/ast.rs
@@ -1,13 +1,59 @@
-pub enum Token {
-    TypeString(String), // "Hello"
-    TypeNumber(i64),    // 10
+use std::collections::HashMap;
 
-    Literal,     // Word
-    SingleQuote, // ''
-    DoubleQuote, // ""
-    Dash,        // -
-    ArrowR,      // >
-    ArrowL,      // <
+pub enum IsiToken {
+    LPAREN,   // (
+    RPAREN,   // )
+    LBRACKET, // [
+    RBRACKET, // ]
+    LBRACE,   // {
+    RBRACE,   // }
+    PIPE,     // |
+    BANG,     // !
+    LARROW,   // <
+    RARROW,   // >
+    QUESTION, // ?
+    ARROW,    // ->
+    SQUOTE,   // ''
+    DQUOTE,   // ""
+
+    INTEGER(i64),
+    FLOAT(f64),
+    STRING(String),
+    KEYWORD(String),
+    TRUE(),
+    FALSE,
+    NIL,
+
+    IDENTIFIER(String),
+    WILDCARD,
+    EOF,
+}
+
+pub enum IsiValue {
+    Integer(i64),
+    Float(f64),
+    String(String),
+    Keyword(String),
+    True,
+    False,
+    Nil,
+
+    // (plus 1 2)
+    Call(Vec<IsiValue>),
+
+    // [1 2 3]
+    Vector(Vec<IsiValue>),
+
+    //{:a 1}
+    Map(HashMap<String, IsiValue>),
+
+    // < (plus 1 2) >
+    Lazy(Box<IsiValue>),
+
+    Function {
+        params: Vec<String>,
+        body: Box<IsiValue>,
+    },
 }
 
 pub struct App {
