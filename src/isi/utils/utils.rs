abad42d5d49eccccbf9df1a74abf3b16b1840b8b
xTheFreeZe <marwin.eder@gmx.de>
2025-11-24 20:35:30 +0100

    Comments

--- src/isi/utils/utils.rs
+++ src/isi/utils/utils.rs
@@ -2,6 +2,7 @@ use std::process::exit;
 
 use colored::Colorize;
 
+/// Throws the given error and exits the program with code `1`
 pub fn print_compile_error(error: String) {
     let message = error.red();
     println!("\nIsi compile error: \n{}", message);
